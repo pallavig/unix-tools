301dc04a65623ffb2c6d5f458bf0dba03adcfd79
Pallavi <pallavig@thoughtworks.com>
2014-01-08 23:49:28 +0530

    head:getting specified number of lines

--- src/pallavig/unixtools/head/Head.java
+++ src/pallavig/unixtools/head/Head.java
@@ -7,15 +7,20 @@ public class Head {
         numberOfRows = 10;
         data = fileData;
     }
+    public Head(String fileData,int requiredNumberOfRows){
+        numberOfRows = requiredNumberOfRows;
+        data = fileData;
+    }
     public String getHeader(){
         String headerData = "";
         String[] linesInData = data.split("\n");
         try {
-             for (int i =0; i < 10; i++){
+             for (int i =0; i < numberOfRows; i++){
                  headerData = headerData.concat(linesInData[i]).concat("\n");
              }
         } catch(Exception e){
         }
-        return headerData.substring(0,headerData.length()-1);
+        headerData = headerData.substring(0,headerData.length()-1);
+        return  headerData;
     }
 }
--- test/pallavig/unixtools/head/HeadTest.java
+++ test/pallavig/unixtools/head/HeadTest.java
@@ -37,4 +37,15 @@ public class HeadTest {
 
         assertEquals(expected,actual);
     }
+
+    @Test
+    public void testHeaderWithSpecificNumberOfLines() throws Exception {
+        String data = "pallavi\nprakash\ngoliwale";
+        String expected = "pallavi\nprakash";
+        Head head = new Head(data,2);
+
+        String actual = head.getHeader();
+
+        assertEquals(expected,actual);
+    }
 }
