4be5584d67e6a4cff1f4f401d99a2aab7294916a
Pallavi <pallavig@thoughtworks.com>
2014-01-09 16:33:57 +0530

    FileContentReader : done with StringBuilder

--- src/reader/FileContentReader.java
+++ src/reader/FileContentReader.java
@@ -5,17 +5,18 @@ import java.io.FileReader;
 
 public class FileContentReader {
     public String readFile(String filename) {
-        String data = "";
+        StringBuilder sb = new StringBuilder();
+        String data;
         String sCurrentLine = "";
         try {
             BufferedReader br = new BufferedReader(new FileReader(filename));
             while ((sCurrentLine = br.readLine()) != null) {
-                data += sCurrentLine + "\n";
+                sb.append(sCurrentLine).append("\n");
             }
         } catch (Exception e) {
             System.err.println("Problem in reading file");
         }
-        data = data.substring(0, data.length() - 1);
+        data = sb.toString().substring(0,sb.length() - 1);
         return data;
     }
 }
