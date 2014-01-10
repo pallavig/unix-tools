832d98ee4d49d5eb4db7edc78f9bc67b2802095e
Pallavi <pallavig@thoughtworks.com>
2014-01-10 14:47:01 +0530

    Tail : library and test cases done

--- src/pallavig/unixtools/tail/Tail.java
+++ src/pallavig/unixtools/tail/Tail.java
@@ -1,7 +1,22 @@
 package pallavig.unixtools.tail;
 
-/**
- * Created by pallavig on 1/10/14.
- */
 public class Tail {
+    private String data;
+    private int numberOfRows;
+    public Tail(String fileData,int requiredNumberOfRows){
+        numberOfRows = requiredNumberOfRows;
+        data = fileData;
+    }
+    public String getLines(){
+        StringBuilder sb = new StringBuilder();
+        String[] linesInData = data.split("\n");
+        int counter = (linesInData.length)-numberOfRows;
+        if(linesInData.length < numberOfRows)
+            return data;
+        while(counter < linesInData.length){
+            sb.append(linesInData[counter]).append("\n");
+            counter++;
+        }
+        return sb.toString().substring(0,sb.length()-1);
+    }
 }
--- test/pallavig/unixtools/tail/TailTest.java
+++ test/pallavig/unixtools/tail/TailTest.java
@@ -2,12 +2,39 @@ package pallavig.unixtools.tail;
 
 import org.junit.Test;
 
-/**
- * Created by pallavig on 1/10/14.
- */
+import static org.junit.Assert.assertEquals;
+
 public class TailTest {
     @Test
     public void testGetLines() throws Exception {
+        String data = "a\nb\nc";
+        Tail tail = new Tail(data,2);
+        String expected = "b\nc";
 
+        String actual = tail.getLines();
+
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void testGetLinesWhenLessLinesArePresentThanExpected(){
+        String data = "a\nb\nc";
+        Tail tail = new Tail(data,10);
+        String expected = "a\nb\nc";
+
+        String actual = tail.getLines();
+
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void testHeaderWhenDataIsEmptyString() throws Exception {
+        String data = "";
+        String expected = "";
+        Tail head = new Tail(data,10);
+
+        String actual = head.getLines();
+
+        assertEquals(expected,actual);
     }
 }
