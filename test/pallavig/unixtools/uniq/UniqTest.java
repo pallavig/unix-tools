66bfff343ca3293ddae54a7e59054576efc9e833
Pallavi <pallavig@thoughtworks.com>
2014-01-16 09:59:08 +0530

    Uniq : test case corrected

--- test/pallavig/unixtools/uniq/UniqTest.java
+++ test/pallavig/unixtools/uniq/UniqTest.java
@@ -7,8 +7,8 @@ import static org.junit.Assert.assertEquals;
 public class UniqTest {
     @Test
     public void testGetUniqLines() throws Exception {
-        String data = "java\nkunal\nkunal\njava";
-        String expected = "java\nkunal\njava\n";
+        String data = "java\r\nkunal\r\nkunal\r\njava";
+        String expected = "java\r\nkunal\r\njava\r\n";
         Uniq uniq = new Uniq(data);
 
         String actual = uniq.getUniqLines();
@@ -18,8 +18,8 @@ public class UniqTest {
 
     @Test
     public void testGetUniqLinesWhenAllLinesAreEqual() throws Exception {
-        String data = "java\njava\njava";
-        String expected = "java\n";
+        String data = "java\r\njava\r\njava";
+        String expected = "java\r\n";
         Uniq uniq = new Uniq(data);
 
         String actual = uniq.getUniqLines();
